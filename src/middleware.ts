139c792701fa8647c9c3136dab3e117073114dbd
rishabh <rishabh.jain@pinnacle.in>
2026-05-05 17:55:51 +0530

    Day 3: Supabase setup with auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+// collab-editor\src\middleware.ts
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/lib/supabase/middleware";
+
+export async function middleware(request: NextRequest) {
+  console.log("Middleware running for:", request.nextUrl.pathname);
+  return await updateSession(request);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization)
+     * - favicon.ico
+     * - public files (svg, png, jpg, jpeg, gif, webp)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
